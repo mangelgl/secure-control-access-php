e47062e59a32ce9a3d36cdc85c9a164bf7952c9d
Miguel Ángel García <magarcial.dev@outlook.es>
2026-02-13 01:03:55 +0100

    Control access for admin role in admin page

---
+++ src/admin.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+if (!isset($_SESSION["email"])) {
+    header("Location: index.php?error=login");
+    exit();
+} elseif (!isset($_SESSION["role"]) || $_SESSION["role"] !== "admin") {
+    header("Location: principal.php?error=not_admin");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Admin dashboard</title>
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Dashboard de administrador</h1>
+    <p>Eres capaz de acceder a esta área restringida porque tienes el rol de administrador.</p>
+    <ul>
+        <li>Información sensible 1</li>
+        <li>Información sensible 2</li>
+        <li>Información sensible 3</li>
+    </ul>
+    <a href="principal.php">Volver</a>
+</body>
+
+</html>
--- src/principal.php
+++ src/principal.php
@@ -1,9 +1,16 @@
 <?php
 session_start();
+$errors = [];
+
 if (!isset($_SESSION["email"])) {
     header("Location: index.php?error=login");
     exit();
 }
+
+if (isset($_REQUEST["error"])) {
+    $errors["not_admin"] = "No tienes permisos para acceder a esta área!";
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -23,6 +30,9 @@ if (!isset($_SESSION["email"])) {
 
 <body>
     <h1>Bienvenido al área restringida</h1>
+    <?php if (isset($errors["not_admin"])): ?>
+        <p><?= $errors["not_admin"] ?></p>
+    <?php endif; ?>
     <p>Has iniciado sesión como <?= $_SESSION["email"] ?></p>
     <a href="logout.php">Cerrar sesión</a>
 </body>
